e852ac275437051a5a489d3b7acefa799a55c32b
muhdHatta <48047949+Muhdhatta@users.noreply.github.com>
2019-05-28 12:56:27 +0800

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import TrelloList from './TrelloList';
+import { connect } from 'react-redux';
+import TrelloActionButton from './TrelloActionButton';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
+import { sort } from '../actions';
+import styled from 'styled-components';
+
+const ListContainer = styled.div`
+    display: flex;
+    flex-Direction: row;
+`;
+
+
+class App extends Component {
+
+    onDragEnd = (result) => {
+        const { destination, source, draggableID, type } = result;
+
+        if(!destination) {
+            return;
+        }
+
+        this.props.dispatch (
+            sort (
+                source.droppableId,
+                destination.droppableId,
+                source.index,
+                destination.index,
+                draggableID,
+                type
+                )
+            );
+    };
+
+    render () {
+        const { lists } = this.props;
+        return (
+        <DragDropContext onDragEnd={this.onDragEnd} >
+        <div className="App">
+            <h2>salam universe</h2>
+            <Droppable
+            droppableId= "all-lists"
+            direction= "horizontal"
+            type= "list"
+            >
+            {provided => (
+                <ListContainer
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                >
+                     {lists.map((list, index) => (
+                    <TrelloList
+                    listID= {list.id}
+                    key={list.id}
+                    title= {list.title}
+                    cards= {list.cards}
+                    index= {index}
+                />
+            ))}
+            {provided.placeholder}
+            <TrelloActionButton list />
+            </ListContainer>
+                )}
+            </Droppable>
+        </div>
+        </DragDropContext>
+        );
+    }
+}
+
+
+const mapStateToProps = state => ({
+    lists: state.lists
+})
+
+export default connect(mapStateToProps)(App);
